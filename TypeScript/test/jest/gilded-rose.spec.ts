0c0dccc2f820afa53957d27ddbf4a7bb8f714907
Dov Rine <dov.rine@gmail.com>
2023-09-08 16:48:28 -0700

    add tests

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,137 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
+
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+  it('should lower the quality and value of every item at the end of every day', () => {
+    // setup
+    const data = {
+      a: { quality: 10, sellIn: 10 },
+      b: { quality: 20, sellIn: 20 },
+      c: { quality: 30, sellIn: 30 },
+    }
+    const items: Item[] = []
+    for (const [name, { quality, sellIn }] of Object.entries(data)) {
+      items.push(new Item(name, sellIn, quality))
+    }
+    const gildedRose = new GildedRose(items)
+
+    // execute
+    gildedRose.updateQuality();
+
+    // assert
+    for (const { name, sellIn, quality } of gildedRose.items) {
+      expect(sellIn).toBe(data[name].sellIn - 1)
+      expect(quality).toBe(data[name].quality - 1)
+      console.log(quality, data[name].quality)
+    }
   });
+  it('should lower the quality twice as fast if sell by date has passed', () => {
+    //setup
+    const data = {
+      a: { quality: 10, sellIn: 0 },
+      b: { quality: 20, sellIn: 0 },
+      c: { quality: 30, sellIn: 0 },
+    }
+    const items: Item[] = []
+    for (const [name, { quality, sellIn }] of Object.entries(data)) {
+      items.push(new Item(name, sellIn, quality))
+    }
+    const gildedRose = new GildedRose(items)
+
+    // execute
+    gildedRose.updateQuality();
+
+    // assert
+    for (const { name, sellIn, quality } of gildedRose.items) {
+      expect(sellIn).toBe(data[name].sellIn - 1)
+      expect(quality).toBe(data[name].quality - 2)
+    }
+  })
+  it('should never lower the quality below zero', () => {
+    //setup
+    const data = {
+      a: { quality: 0, sellIn: 0 },
+      b: { quality: 0, sellIn: 0 },
+      c: { quality: 0, sellIn: 0 },
+    }
+    const items: Item[] = []
+    for (const [name, { quality, sellIn }] of Object.entries(data)) {
+      items.push(new Item(name, sellIn, quality))
+    }
+    const gildedRose = new GildedRose(items)
+
+    // execute
+    gildedRose.updateQuality();
+
+    // assert
+    for (const { name, quality } of gildedRose.items) {
+      expect(quality).toBe(0)
+    }
+  })
+  it('should never set the quality higher than 50', () => {
+    //setup
+    const data = {
+      "Aged Brie": { quality: 50, sellIn: 10 },
+    }
+    const items: Item[] = []
+    for (const [name, { quality, sellIn }] of Object.entries(data)) {
+      items.push(new Item(name, sellIn, quality))
+    }
+    const gildedRose = new GildedRose(items)
+
+    // execute
+    gildedRose.updateQuality();
+
+    // assert
+    for (const { quality } of gildedRose.items) {
+      expect(quality).toBe(50)
+    }
+  })
 });
+
+describe("Item Exceptions", () => {
+  describe("Legendary Items", () => {
+    it("never has to be sold", () => {
+      //setup
+      const data = {
+        "Sulfuras, Hand of Ragnaros": { quality: 49, sellIn: 10 },
+      }
+      const items: Item[] = []
+      for (const [name, { quality, sellIn }] of Object.entries(data)) {
+        items.push(new Item(name, sellIn, quality))
+      }
+      const gildedRose = new GildedRose(items)
+
+      // execute
+      gildedRose.updateQuality();
+
+      // assert
+      for (const { name, sellIn } of gildedRose.items) {
+        expect(sellIn).toBe(10)
+      }
+    })
+    it.todo("never decreases in quality")
+  })
+  describe("Aged Brie", () => {
+    it("should increase quality every day", () => {
+      //setup
+      const data = {
+        "Aged Brie": { quality: 49, sellIn: 10 },
+      }
+      const items: Item[] = []
+      for (const [name, { quality, sellIn }] of Object.entries(data)) {
+        items.push(new Item(name, sellIn, quality))
+      }
+      const gildedRose = new GildedRose(items)
+
+      // execute
+      gildedRose.updateQuality();
+
+      // assert
+      for (const { name, quality } of gildedRose.items) {
+        expect(quality).toBe(50)
+      }
+
+    })
+  })
+})
