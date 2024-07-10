11c97c115c8ad2e87cf5baceccd471f7626cc788
Dov Rine <dov.rine@gmail.com>
2024-07-09 21:42:20 -0700

    python solution

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,39 +1,59 @@
 # -*- coding: utf-8 -*-
 
 class GildedRose(object):
-
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            update_func = item_update_functions.get(
+                item.name, update_regular_item)
+            update_func(item)
+
+
+def decrease_quality(item, amount=1):
+    item.quality = max(0, item.quality - amount)
+
+
+def increase_quality(item, amount=1):
+    item.quality = min(50, item.quality + amount)
+
+
+def update_regular_item(item):
+    decrease_quality(item)
+    item.sell_in -= 1
+    if item.sell_in < 0:
+        decrease_quality(item)
+
+
+def update_aged_brie(item):
+    increase_quality(item)
+    item.sell_in -= 1
+    if item.sell_in < 0:
+        increase_quality(item)
+
+
+def update_backstage_pass(item):
+    if item.sell_in > 10:
+        increase_quality(item)
+    elif item.sell_in > 5:
+        increase_quality(item, amount=2)
+    elif item.sell_in > 0:
+        increase_quality(item, amount=3)
+    else:
+        item.quality = 0
+    item.sell_in -= 1
+
+
+def update_sulfuras(item):
+    pass
+
+
+item_update_functions = {
+    "Aged Brie": update_aged_brie,
+    "Backstage passes to a TAFKAL80ETC concert": update_backstage_pass,
+    "Sulfuras, Hand of Ragnaros": update_sulfuras
+}
 
 
 class Item:
